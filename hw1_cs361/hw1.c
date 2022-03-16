5829ca272e93eb01a0a6876409001b6e00cc8b8d
stevenmach123 <machgiakhang@gmail.com>
2022-03-16 00:40:18 -0500

    a1

---
+++ hw1_cs361/hw1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "hw1.h"
+
+static int I_have_written(char* a){
+
+}
+static int the_code[3] = {4,5};
+static void that_you_needed(){
+        static int to_compile=1;
+
+
+}
+
+static void and_which(){
+        static int has_a_bunch_of=1;
+        static int ridiculous =1;
+
+}
+
+void sides_and(){
+  static int symbols[2];
+}
+void Forgive_me(){}
+int they_are_arbitrary;
+int so_random[2] = {1,2};
+void and_so_varied(){};
+int main(int argc,char* argv[]){
+
+       printf("fd");
+        return 0;
+}
